f7e63de524eba73df58f2fdfb4d8e1b717ab5bec
Pedro Henrique Guimarães Carvalho <pedrohenriqueguimaraes1515@gmail.com>
2023-10-24 23:38:13 -0300

    Add selection

--- Código/index.js
+++ Código/index.js
@@ -6,8 +6,18 @@ function jsonData(url){
 
 async function getAPI() {
     const data = await jsonData('https://jsonplaceholder.typicode.com/users')
-    console.log(data)
+    userSelection(data)
 }
 
 getAPI()
+
+function userSelection(data){
+    let select = document.getElementById("users")
+    
+    data.map((item) =>{
+        let option = document.createElement('option')
+        option.textContent = item.name
+        select.appendChild(option)
+    })
+}
 
